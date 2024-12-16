3fdc449e0f361e2916df13026f8bebaf1ba2b344
gnsndr <ginasunandar07@gmail.com>
2024-12-16 22:13:39 +0700

    siperpus

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -11,10 +11,9 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     <x-primary-button tag="a" href="{{ route('book.create') }}">Tambah Data Buku</x-primary-button>
-                    <x-primary-button tag="a" href="{{ route('book.print') }}">Print PDF</x-primary-button>
+                    <x-primary-button tag="a" href="{{ route('book.print') }}">Cetak PDF</x-primary-button>
                     <x-primary-button tag="a" href="{{ route('book.export') }}" target="_blank">Export Excel</x-primary-button>
-                    <x-primary-button tag="a" href="{{ route('book.import') }}" target="_blank">Import
-                        Excel</x-primary-button>
+                    <x-primary-button tag="a" href="{{ route('book.import') }}" target="_blank">Import Excel</x-primary-button>
 
                     <x-table>
                         <x-slot name="header">
